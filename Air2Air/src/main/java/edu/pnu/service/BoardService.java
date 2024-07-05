b2f5ad7f8b31b7e75ed1480cf244633c7a41be37
LeeYeongin <youngin5757@gmail.com>
2024-07-05 18:07:36 +0900

    0705 답변 서비스 작성중

--- Air2Air/src/main/java/edu/pnu/service/BoardService.java
+++ Air2Air/src/main/java/edu/pnu/service/BoardService.java
@@ -8,8 +8,10 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import edu.pnu.ReqDTO.AnswerFormDTO;
 import edu.pnu.ReqDTO.QuestionFormDTO;
 import edu.pnu.ResDTO.QuestionSimpleDTO;
+import edu.pnu.domain.AnswerBoard;
 import edu.pnu.domain.Member;
 import edu.pnu.domain.QuestionBoard;
 import edu.pnu.persistence.AnswerBoardRepository;
@@ -24,7 +26,7 @@ public class BoardService {
 	AnswerBoardRepository answerRepository;
 	@Autowired
 	MemberRepository memberRepository;
-	
+
 	public List<QuestionSimpleDTO> getBoardList() {
 		List<QuestionBoard> questionList = questionRepository.findAll(Sort.by(Direction.DESC, "seq"));
 		List<QuestionSimpleDTO> questionDTOList = questionList.stream().map(QuestionSimpleDTO::convertToDTO)
@@ -33,21 +35,39 @@ public class BoardService {
 	}
 
 	public boolean addQuestion(QuestionFormDTO question) {
-		Member member = memberRepository.findByUsername(question.getUsername())
-								.orElse(null);
-		
-		if(member == null)
+		Member member = memberRepository.findByUsername(question.getUsername()).orElse(null);
+
+		if (member == null)
 			return false;
-				
+
 		QuestionBoard questionBoard = QuestionBoard.builder()
-				.member(member)
-				.title(question.getTitle())
-				.content(question.getContent())
-				.questionType(QuestionBoard.stringToEnum(question.getType()))
-				.build();
-		
+									.member(member).title(question.getTitle())
+									.content(question.getContent())
+									.questionType(QuestionBoard.stringToEnum(question.getType()))
+									.build();
+
 		questionRepository.save(questionBoard);
+
+		return true;
+	}
+
+	public boolean addAnswer(AnswerFormDTO answer) {
+		Member member = memberRepository.findByUsername(answer.getUsername()).orElse(null);
+		if (member == null)
+			return false;
 		
+		QuestionBoard question = questionRepository.findById(Long.parseLong(answer.getQuestionId())).get();
+		if (question == null)
+			return false;
+		
+		AnswerBoard answerBoard = AnswerBoard.builder()
+								.content(answer.getContent())
+								.member(member)
+								.questionBoard(question)
+								.build();
+								
+		answerRepository.save(answerBoard);
+
 		return true;
 	}
 }
